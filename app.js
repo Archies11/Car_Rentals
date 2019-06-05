5a1a3d6b4c8313195a5e58a9aa925387d5af72b6
Archies11 <iit2018018@iiita.ac.in>
2019-06-06 02:44:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const vm = new Vue ({
+    el : '#app' ,
+    data : {
+        results: []
+    },
+    mounted() {
+        axios.get("https://api.sheety.co/311576ae-321a-43e3-9a5b-61b3ac373d85")
+        .then(response => {this.results = response.data })
+    }
+});
