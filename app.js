e95924204e16cf9f177ea4133b23fcc9d882b389
Archies11 <iit2018018@iiita.ac.in>
2019-06-06 23:43:17 +0530

    got images to show

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const vm = new Vue ({
     el : '#app' ,
     data : {
-        results: []
+        results: [],
     },
     mounted() {
         axios.get("https://api.sheety.co/311576ae-321a-43e3-9a5b-61b3ac373d85")
