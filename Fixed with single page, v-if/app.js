b6e9159a298b582091ce3b45aa9d73fb151b931f
Archies11 <iit2018018@iiita.ac.in>
2019-06-16 17:33:55 +0530

    one page solution

--- Fixed with single page, v-if/app.js
+++ Fixed with single page, v-if/app.js
@@ -7,19 +7,21 @@ const vm = new Vue ({
         results: [],
         locations: LOCATIONS.split(','),
         location: 'Kormangala',
+        showResults: false
     },
     mounted() {
         this.getPosts(this.location);
     },
     methods: {
         getPosts(location) {
+            this.showResults = false
             axios.get("https://api.sheety.co/311576ae-321a-43e3-9a5b-61b3ac373d85")
             .then(response => {this.initial_results = response.data });
             let posts = this.initial_results;
             posts.map(post => {
                 if(post.location === this.location){
-                    console.log('aaa');
                     this.results.push({name:post.name,photo:post.photo,price:post.price,location:post.location,seats:post.seats,fuel_type:post.fuel_type});
+                    this.showResults = true
                 }
             });
         } 
