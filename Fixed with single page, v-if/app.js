6023b2f5e1031e04200032009ccf61c58eb1bce8
Archies11 <iit2018018@iiita.ac.in>
2019-06-18 21:57:52 +0530

    date kinda

--- Fixed with single page, v-if/app.js
+++ Fixed with single page, v-if/app.js
@@ -1,5 +1,5 @@
 const LOCATIONS = "Koramangala,HSR Layout,Indiranagar";
-const DATES = "Mon,Tues,Wednes,Thurs,Fri,Satur,Sun";
+const DATES = "Mon,Tue,Wed,Thu,Fri,Sat,Sun";
 
 const vm = new Vue ({
     el : '#app' ,
@@ -10,21 +10,30 @@ const vm = new Vue ({
         location: 'Kormangala',
         dates: DATES.split(','),
         date: '',
-        showResults: false
+        showResults: false,
+        present: false
     },
     mounted() {
-        this.getPosts(this.location);
+        this.getPosts(this.location,this.date);
     },
     methods: {
-        getPosts(location) {
+        getPosts(location,date) {
             this.showResults = false
+            this.present = true
             axios.get("https://api.sheety.co/311576ae-321a-43e3-9a5b-61b3ac373d85")
             .then(response => {this.initial_results = response.data });
             let posts = this.initial_results;
             posts.map(post => {
                 if(post.location === this.location){
                     this.results.push({name:post.name,photo:post.photo,price:post.price,location:post.location,seats:post.seats,fuel_type:post.fuel_type});
-                    this.showResults = true;}
+                    this.showResults = true;
+                    let avails = post.availabilty.split(',');
+                    avails.map(avail => {
+                        if(avail==this.date){
+                            this.present = true;
+                        }
+                    });
+                }
             });
         } 
     }
